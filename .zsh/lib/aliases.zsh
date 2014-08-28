9ee6b535f1653bf5e60eee2c9d48df2ec0bc7e54
arl4223 <arl4223@packetjunkie.net>
2014-08-28 15:21:35 +0200

    added aliasz for python

--- .zsh/lib/aliases.zsh
+++ .zsh/lib/aliases.zsh
@@ -1,7 +1,4 @@
 alias ll='ls -la'
 alias lh='ls -lah'
 alias ...='cd ../..'
-
-srvfind () {
-    cat ~/workspace/wm/puttysessions.csv | grep "$1" | awk -F, '{print $NF}' | sed 's/\"//g'
-}
+alias python='python3'
