e29a137c59d05ae041d88ad9c625b5a269a698c1
arl4223 <arl4223@donna.local>
2015-07-19 17:14:34 +0200

    added small install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+HOME='/Users/arl4223'
+SELF=$(pwd)
+
+
+ln -s $SELF/.git $HOME/.git 
+ln -s $SELF/.gitignore $HOME/.gitignore 
+ln -s $SELF/.vim $HOME/.vim 
+ln -s $SELF/.vimrc $HOME/.vimrc 
+ln -s $SELF/.zsh $HOME/.zsh 
+ln -s $SELF/.zshrc $HOME/.zshrc 
